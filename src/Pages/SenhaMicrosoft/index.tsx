287e9126d5df84e46d75e268eb4c1cb52b79e4f2
André Silva <andre06x@gmail.com>
2023-01-14 10:11:14 -0300

    refatorando loading senha

--- src/Pages/SenhaMicrosoft/index.tsx
+++ src/Pages/SenhaMicrosoft/index.tsx
@@ -1,97 +1,130 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View, TouchableHighlight, TouchableOpacity, TextInput } from 'react-native';
+import React, { useEffect, useCallback } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Inter_700Bold, Inter_500Medium, Inter_600SemiBold, useFonts } from '@expo-google-fonts/inter';
-import AppLoading from 'expo-app-loading';
-import { Colors } from "react-native/Libraries/NewAppScreen";
-import { Ionicons } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+import {
+  Inter_700Bold,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { Ionicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import * as SplashScreen from "expo-splash-screen";
 
-const SenhaMicrosoft = ({ navigation, route }: { navigation: any, route: any }) => {
-  console.log(route.params)
-
+const SenhaMicrosoft = ({
+  navigation,
+  route,
+}: {
+  navigation: any;
+  route: any;
+}) => {
   let [fontsLoaded] = useFonts({
     Inter_600SemiBold,
     Inter_500Medium,
-    Inter_700Bold
+    Inter_700Bold,
   });
 
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
-    return <AppLoading />;
-  }
+    return null;
+  } else
+    return (
+      <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+        <Image source={require("./microsoft_logo.png")} />
 
-  else return (
-    <SafeAreaView style={styles.container}>
+        <View style={styles.view_email}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <AntDesign
+              style={{ marginRight: 5 }}
+              name="arrowleft"
+              size={17}
+              color="black"
+            />
+          </TouchableOpacity>
 
-      <Image source={require('./microsoft_logo.png')} />
+          <Text>{route.params}</Text>
+        </View>
 
-      <View style={styles.view_email}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <AntDesign 
-            style={{marginRight: 5}} 
-            name="arrowleft" 
-            size={17} 
-            color="black"
-          />
+        <Text style={styles.titulo}>Insira a senha</Text>
+
+        <TextInput style={styles.input} placeholder="Senha" />
+
+        <TouchableOpacity>
+          <Text style={styles.nao_consegue}>
+            Não consegue acessar sua conta?
+          </Text>
         </TouchableOpacity>
 
-        <Text>{route.params}</Text>
-      </View>
+        <View style={styles.container_button}>
+          <TouchableOpacity
+            style={styles.botao_avancar}
+            onPress={() => navigation.navigate("Home")}
+          >
+            <Text style={styles.text_avancar}>Avançar</Text>
+          </TouchableOpacity>
+        </View>
 
-      <Text style={styles.titulo}>Insira a senha</Text>
-
-      <TextInput style={styles.input} placeholder="Senha" />
-
-      <TouchableOpacity>
-        <Text style={styles.nao_consegue}>Não consegue acessar sua conta?</Text>
-      </TouchableOpacity>
-
-      <View style={styles.container_button}>
-        <TouchableOpacity style={styles.botao_avancar} onPress={() => navigation.navigate("Home")}>
-          <Text style={styles.text_avancar}>Avançar</Text>
-        </TouchableOpacity>
-      </View>
-
-      <TouchableOpacity style={styles.botao_opcoes}>
-        <Ionicons name="key-outline" size={24} color="black" />
-        <Text style={styles.text_opcoes}>Opções de entrada</Text>
-      </TouchableOpacity>
-
-      <View style={styles.outros}>
-        <TouchableOpacity style={{ width: "33%" }}>
-          <Text style={styles.textOutros}>Termos de uso</Text>
+        <TouchableOpacity style={styles.botao_opcoes}>
+          <Ionicons name="key-outline" size={24} color="black" />
+          <Text style={styles.text_opcoes}>Opções de entrada</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={{ width: "43%" }}>
-          <Text style={styles.textOutros}>Privacidade e cookies</Text>
-        </TouchableOpacity>
+        <View style={styles.outros}>
+          <TouchableOpacity style={{ width: "33%" }}>
+            <Text style={styles.textOutros}>Termos de uso</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={{ width: "33%" }}>
-          <Text style={styles.textOutros}>...</Text>
-        </TouchableOpacity>
-      </View>
+          <TouchableOpacity style={{ width: "43%" }}>
+            <Text style={styles.textOutros}>Privacidade e cookies</Text>
+          </TouchableOpacity>
 
-      <StatusBar style="dark" />
-    </SafeAreaView>
-  );
-}
+          <TouchableOpacity style={{ width: "33%" }}>
+            <Text style={styles.textOutros}>...</Text>
+          </TouchableOpacity>
+        </View>
+
+        <StatusBar style="dark" />
+      </SafeAreaView>
+    );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    padding: 30
+    backgroundColor: "#fff",
+    padding: 30,
   },
   logo: {
     width: 220,
     height: 60,
-    marginBottom: 20
+    marginBottom: 20,
   },
   titulo: {
     fontSize: 28,
     fontFamily: "Inter_700Bold",
     marginVertical: 5,
-    color: "#1B1B1B"
+    color: "#1B1B1B",
   },
 
   input: {
@@ -105,7 +138,7 @@ const styles = StyleSheet.create({
   },
 
   nao_consegue: {
-    color: "#0067b8"
+    color: "#0067b8",
   },
 
   container_button: {
@@ -120,11 +153,11 @@ const styles = StyleSheet.create({
     paddingVertical: 4,
     paddingHorizontal: 12,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
 
   text_avancar: {
-    color: "#fff"
+    color: "#fff",
   },
 
   botao_opcoes: {
@@ -133,11 +166,11 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     alignItems: "center",
     borderWidth: 1,
-    color: "black"
+    color: "black",
   },
 
   text_opcoes: {
-    marginLeft: 10
+    marginLeft: 10,
   },
 
   email: {
@@ -145,7 +178,6 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     textAlign: "center",
     color: "#999",
-
   },
   botao_entrar: {
     marginVertical: 34,
@@ -154,7 +186,7 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     backgroundColor: "#24BCCA",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   botao_ajuda: {
     position: "absolute",
@@ -167,7 +199,6 @@ const styles = StyleSheet.create({
     backgroundColor: "transparent",
     justifyContent: "center",
     alignItems: "center",
-
   },
 
   outros: {
@@ -176,13 +207,13 @@ const styles = StyleSheet.create({
     left: 30,
     flexDirection: "row",
     display: "flex",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
 
   textOutros: {
     color: "#747474",
     marginRight: 10,
-    fontSize: 12
+    fontSize: 12,
   },
 
   view_email: {
@@ -190,8 +221,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginVertical: 15,
   },
-
-
 });
 
-export { SenhaMicrosoft }
+export { SenhaMicrosoft };
